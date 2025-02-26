bd07943437b92190de169ce0277df0c5bf48aa74
keebertron <165371780+keebertron@users.noreply.github.com>
2025-02-26 21:36:07 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /home/$USER/prometheus
+docker rm -f /node_exporter
+
+docker run -d \
+  --net="host" \
+  --name node_exporter \
+  -v "/:/host:ro,rslave" \
+  quay.io/prometheus/node-exporter:latest \
+  --path.rootfs=/host
+
+docker compose up -d
+echo "KravchenkoVA 5" | curl --data-binary @- http://localhost:9091/metrics/job/net>
+ss -tlpn
